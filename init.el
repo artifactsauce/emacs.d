a7c3c88b5ff19804a755bac171bbca60ed3ec705
Kenji Akiyama <artifactsauce@gmail.com>
2014-06-23 20:57:31 +0900

    add file type to ruby-mode

--- init.el
+++ init.el
@@ -151,6 +151,8 @@
 (autoload 'ruby-mode "ruby-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
 
 ;; python
