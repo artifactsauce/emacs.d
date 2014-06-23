9b102530e3ec86bc209d2cd8b799c7dc3b6d3da8
Kenji Akiyama <artifactsauce@gmail.com>
2014-06-23 20:56:46 +0900

    add yasnipet setting.

--- init.el
+++ init.el
@@ -64,6 +64,30 @@
 ;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+;; yasnippet
+(require 'yasnippet)
+;; (require 'yasnippet-bundle)
+(yas-global-mode 1)
+(defun shk-yas/helm-prompt (prompt choices &optional display-fn)
+  "Use helm to select a snippet. Put this into `yas/prompt-functions.'"
+  (interactive)
+  (setq display-fn (or display-fn 'identity))
+  (if (require 'helm-config)
+      (let (tmpsource cands result rmap)
+        (setq cands (mapcar (lambda (x) (funcall display-fn x)) choices))
+        (setq rmap (mapcar (lambda (x) (cons (funcall display-fn x) x)) choices))
+        (setq tmpsource
+              (list
+               (cons 'name prompt)
+               (cons 'candidates cands)
+               '(action . (("Expand" . (lambda (selection) selection))))
+               ))
+        (setq result (helm-other-buffer '(tmpsource) "*helm-select-yasnippet"))
+        (if (null result)
+            (signal 'quit "user quit!")
+          (cdr (assoc result rmap))))
+    nil))
+
 ;; perl
 (autoload 'cperl-mode "cperl-mode" nil t)
 (add-to-list 'auto-mode-alist '("\.\([pP][Llm]\|al\|t\)\'" . cperl-mode))
