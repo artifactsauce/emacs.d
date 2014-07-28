b10720324a58d8a8a04a04d69923eb3b063fe8bd
Kenji Akiyama <artifactsauce@gmail.com>
2014-07-28 09:55:59 +0900

    share clipboard with Mac OS X.

--- init.el
+++ init.el
@@ -28,6 +28,23 @@
  )
 (setq default-directory "~/")
 (electric-pair-mode 1)
+; Clipboard copy and paste
+(when window-system
+  (global-set-key "\C-w" 'clipboard-kill-region)
+  (global-set-key "\M-w" 'clipboard-kill-ring-save)
+  (global-set-key "\C-y" 'clipboard-yank)
+  )
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+
+(setq interprogram-cut-function 'paste-to-osx)
+(setq interprogram-paste-function 'copy-from-osx)
 
 ; Font setting
 (when window-system
