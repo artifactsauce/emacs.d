94d43af7dc1a115b83d92f5b35593508b8067377
Kenji Akiyama <artifactsauce@gmail.com>
2016-09-11 18:22:37 +0900

    add helm settings

--- init.el
+++ init.el
@@ -65,6 +65,10 @@
 ;; helm
 (helm-mode 1)
 (global-set-key (kbd "C-c h") 'helm-mini)
+(global-set-key (kbd "C-x b") 'helm-for-files)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
 ;; auto-complete
 (global-auto-complete-mode t)
